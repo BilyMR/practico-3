de4820a0c324ca329040c1edf3556bcd7c08e3b6
Eduardo Miranda <bilymirandarivero@gmail.com>
2025-04-06 00:02:57 -0400

    Update ex2.cpp
    redo del ejercicio

--- ex2.cpp
+++ ex2.cpp
@@ -5,20 +5,20 @@ int main()
     std::string a;
     std::getline(std::cin, a);
     
-    std::string word = "[";
+    std::string word = "";
     
     for(int i = 0; i < a.length(); i++) {
         
         if(a[i] != 32) {
             word += a[i];
         } else if (a[i] == 32){
-            word += "]\n[";
+            std::cout<<"["<<word<<"]"<<std::endl;
+            word= "";
+        }
+        if(i == a.length()-1) {
+            std::cout<<"["<<word<<"]"<<std::endl;
         }
     }
     
-    word += "]";
-    
-    std::cout<<word;
-
     return 0;
 }
